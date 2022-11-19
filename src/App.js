ebff0fb10d7f139d5a1ad283806b119fe8980d34
MolinaMS95 <matheusmolina95@gmail.com>
2022-11-18 22:06:29 -0300

    Estrutura inicial do projeto

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import GlobalStyle from "./assets/GlobalStyle";
+import Login from "./pages/Login/Login";
+import SignUp from "./pages/SignUp/SignUp";
+import Registers from "./pages/Registers/Registers";
+import Insert from "./pages/Insert/Insert";
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <GlobalStyle />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Login />} />
+          <Route path="/cadastro" element={<SignUp />} />
+          <Route path="/registros" element={<Registers />} />
+          <Route path="/inserir" element={<Insert />} />
+        </Routes>
+      </BrowserRouter>
+    </>
   );
 }
-
-export default App;
